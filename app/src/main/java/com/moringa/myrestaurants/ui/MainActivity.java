1f9fc3fcc1ad72c5361f2cbf07453ab9b6e43fa5
George Okumu <gokumu12@gmail.com>
2021-09-15 18:42:51 +0300

    Added Event listeners to listen to updated data of our node.

--- app/src/main/java/com/moringa/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/moringa/myrestaurants/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moringa.myrestaurants.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -17,8 +18,11 @@ import android.widget.Toast;
 import com.google.android.material.tabs.TabLayout;
 //import com.google.firebase.database.DatabaseReference;
 //import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.moringa.myrestaurants.Constants;
 import com.moringa.myrestaurants.R;
 
@@ -30,16 +34,12 @@ import static com.moringa.myrestaurants.Constants.FIREBASE_LOCATION_SEARCHED_LOC
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = " in MainActivity";
-//    private Button mFindRestaurantsButton;
-//    private EditText mLocationEditText;
 
+    //Initializing Event Listener for listening to the location node
+    private ValueEventListener valueEventListener;
 //Initializing the firebase referrence
    DatabaseReference reference;
-    
 
-
-////
-//    private DatabaseReference mSearchedLocationReference;
 
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
@@ -55,6 +55,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .getInstance()
                 .getReference()
                 .child(FIREBASE_LOCATION_SEARCHED_LOCATION);
+
+        //Call the addValueEventListener to the database reference,
+        // Assign the valueEventListener to the reference of the location so that it
+        //Should not return nullPointerException of the listener.
+        //Snapshot is now stored in the listener so that it can be destroyed.
+         valueEventListener = reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Snapshot is the data that changed on our node
+                //Basically we are adding listener to check the data which is being saved in our node
+                for (DataSnapshot searchedSnapshot : snapshot.getChildren()){
+                    //Getting the value of the updated location in our node.
+                    String location = searchedSnapshot.getValue().toString();
+                    Log.d(TAG, "onDataChange: This is the data that is updated/added into our method: " + location);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "onCancelled: " +  error );
+
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -89,4 +112,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // removing the eventListener from the node
+        reference.removeEventListener(valueEventListener);
+        Log.i(TAG, "onDestroy: App destroyed successfully");
+    }
 }
